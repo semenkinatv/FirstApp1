cef38690d0c9dd9deb76facd4cb071b9003b482e
semenkinatv <semenkinatv@gmail.com>
2021-02-24 00:26:01 +0300

    My name is

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,11 @@ class MainClass
 		myName = "Tatiana";
 
 		Console.WriteLine(myName);
-
+		Console.WriteLine("Привет,\n мир!");
+		Console.WriteLine("\t My name is \n {0}", myName);
+		Console.WriteLine('\u0040');
+		Console.WriteLine('\x23');
+		Console.WriteLine('\u005D');
 		Console.ReadKey();
 	}
 }
